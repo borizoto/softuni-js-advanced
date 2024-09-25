bf47e91b33f1cbad14a137c3f65512a4f084d891
Boris Kalapchiev <164948528+borizoto@users.noreply.github.com>
2024-09-26 00:38:35 +0300

    Add files via upload

---
+++ 01. Syntax, Functions and Statements - Lab/02-stringLength.js
@@ -0,0 +1,6 @@
+function stringLength(string1, string2, string3) {
+    const sum = string1.length + string2.length + string3.length;
+    const avg = Math.floor(sum / 3);
+    console.log(sum);
+    console.log(avg);
+ }
